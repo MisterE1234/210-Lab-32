96601f87367e9b2ef6a3701f107c5df29d4adfd2
MisterE1234 <MisterE1234@outlook.com>
2025-11-13 15:48:46 -0800

    3rd Commit

--- main.cpp
+++ main.cpp
@@ -5,10 +5,17 @@
 #include <deque>
 #include "Car.h"
 
+int LEAVE_CHANCE = 55, JOIN_CHANCE = 45;
+
 void displayLine(deque<Car>);
 
 int main(){
+    //Using a random seed:
+    srand(time(0));
 
+    bool debug = true;
+    int chance;
+    int opNum = 1;
     deque<Car> tollLine;
 
     //Initializing the line:
@@ -18,6 +25,18 @@ int main(){
     //Displaying the Initial line:
     cout << "Initial Queue:\n";
     displayLine(tollLine);
+
+    //A while loop that will only exit when the deque is empty:
+    while(!tollLine.empty() && debug){
+        //using a random numnber between 100 and 1 to get the 
+        chance = rand()%100 + 1;
+        if(chance < JOIN_CHANCE){
+            tollLine.push_back(Car());
+            cout << "Time: " << opNum << " Operation: Car Joined: ";
+        }
+
+        opNum++;
+    }
 
 
     return 0;
