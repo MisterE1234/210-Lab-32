b3dd65119c293250340ca662cc5a1d54b46d1aac
MisterE1234 <MisterE1234@outlook.com>
2025-11-13 15:25:39 -0800

    2nd Comit

--- main.cpp
+++ main.cpp
@@ -1,2 +1,22 @@
 //COMSC-210|Ethan Aylard|Lab-32
 //IDE used: Visual Studio Code
+
+#include <iostream>
+#include <deque>
+#include "Car.h"
+
+
+
+int main(){
+
+    deque<Car> tollLine;
+
+    //Initializing the line:
+    tollLine.push_back(Car());
+    tollLine.push_back(Car());
+
+    //displaying the Initial line:
+
+
+    return 0;
+}
