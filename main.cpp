5cc7f9a883b6002e07888ee22c6435d45db18fc4
MisterE1234 <MisterE1234@outlook.com>
2025-11-13 16:12:01 -0800

    5th commit

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <deque>
 #include "Car.h"
 
-int SPLIT_PROB = 45; bool debug = true;
+int SPLIT_PROB = 45; bool debug = false;
 
 void displayLine(deque<Car>);
 
@@ -13,42 +13,47 @@ int main(){
     //Using a random seed:
     srand(time(0));
 
+    //a bool variable that determines if the while loop continues
     bool loop = true;
 
-    int chance;
-    int opNum = 1;
+    int chance; // a number that will contain a random number between 1 and 100.
+    int opNum = 1; // the number that is the current operation
     deque<Car> tollLine;
 
     //Initializing the line:
     tollLine.push_back(Car());
     tollLine.push_back(Car());
 
-    //Displaying the Initial line:
+    //Displaying the initial line:
     cout << "Initial Queue:\n";
     displayLine(tollLine);
 
-    Car tempElement;
 
     //A while loop that will only exit when the deque is empty: It will also skip the loop if 
     while(loop){
         //using a random numnber between 100 and 1 to get what event plays out:
         chance = rand()%100 + 1;
 
-        if(chance < SPLIT_PROB){ // if the random number is less than SPLIT_PROB (45)
-            tollLine.push_back(Car());
+        if(chance < SPLIT_PROB){ // if the random number is less than SPLIT_PROB (45) so that is has a 45% chance to happen.
+            tollLine.push_back(Car()); //a new car joins the queue
 
-            cout << "Time: " << opNum << " Operation: Car Joined: ";
+            cout << "Time: Operation " << opNum << " : Car Joined: ";
             tollLine.back().print();
         }
-        //if the 
-        else if(chance >= SPLIT_PROB){
-            cout << "Time: " << opNum << " Operation: Car Paid: ";
+        //if the random number is more or equal to than SPLIT_PROB (45) sor that it has a 55% chance to happen.
+        else if(chance >= SPLIT_PROB){//The first car pays its toll
+            cout << "Time: Operation " << opNum << " : Car Paid: ";
             tollLine.front().print();
 
             tollLine.pop_front();
         }
 
+        //Displaying the current line:
+        cout << "Queue:\n";
+        displayLine(tollLine);
+
         opNum++;
+        //if the deque or debug is true end the loop:
         if (tollLine.empty()|| debug){
             loop = false;
         }
